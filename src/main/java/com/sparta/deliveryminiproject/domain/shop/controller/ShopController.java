fa25fd5f148490ec59bf67c35f239168cbdb13f1
k-jaehyun <wogus146@gmail.com>
2024-11-12 22:58:44 +0900

    fix : @RequestBody -> @ModelAttribute 변경

--- src/main/java/com/sparta/deliveryminiproject/domain/shop/controller/ShopController.java
+++ src/main/java/com/sparta/deliveryminiproject/domain/shop/controller/ShopController.java
@@ -14,10 +14,10 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +32,7 @@ public class ShopController {
   @Secured({Authority.MASTER, Authority.MANAGER})
   @PostMapping
   public ApiResponse createShop(
-      @RequestBody ShopRequestDto shopRequestDto) {
+      @ModelAttribute ShopRequestDto shopRequestDto) {
 
     ShopResponseDto shopResponseDto = shopService.createShop(shopRequestDto);
 
@@ -66,7 +66,7 @@ public class ShopController {
   @PutMapping("/{shopId}")
   public ApiResponse updateShop(
       @PathVariable UUID shopId,
-      @Valid @RequestBody ShopRequestDto shopRequestDto) {
+      @Valid @ModelAttribute ShopRequestDto shopRequestDto) {
 
     ShopResponseDto shopResponseDto = shopService.updateShop(shopId, shopRequestDto);
 
