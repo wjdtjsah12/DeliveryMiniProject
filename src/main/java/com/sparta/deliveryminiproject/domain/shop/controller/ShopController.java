a5f0294265ad74ea1077a49152b80379bf0e728d
k-jaehyun <wogus146@gmail.com>
2024-11-12 16:16:52 +0900

    fix: Controller _Response 수정

--- src/main/java/com/sparta/deliveryminiproject/domain/shop/controller/ShopController.java
+++ src/main/java/com/sparta/deliveryminiproject/domain/shop/controller/ShopController.java
@@ -11,8 +11,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -33,27 +31,25 @@ public class ShopController {
 
   @Secured({Authority.MASTER, Authority.MANAGER})
   @PostMapping
-  public ResponseEntity<ApiResponse> createShop(
+  public ApiResponse createShop(
       @RequestBody ShopRequestDto shopRequestDto) {
 
     ShopResponseDto shopResponseDto = shopService.createShop(shopRequestDto);
 
-    return ResponseEntity.status(HttpStatus.OK)
-        .body(new ApiResponse("가게 생성 성공", HttpStatus.OK.value(), shopResponseDto));
+    return ApiResponse.success(shopResponseDto);
   }
 
   @GetMapping("/{shopId}")
-  public ResponseEntity<ApiResponse> getShop(
+  public ApiResponse getShop(
       @PathVariable Long shopId) {
 
     ShopResponseDto shopResponseDto = shopService.getShop(shopId);
 
-    return ResponseEntity.status(HttpStatus.OK)
-        .body(new ApiResponse("가게 조회 성공", HttpStatus.OK.value(), shopResponseDto));
+    return ApiResponse.success(shopResponseDto);
   }
 
   @GetMapping
-  public ResponseEntity<ApiResponse<Page<ShopResponseDto>>> getShopList(
+  public ApiResponse<Page<ShopResponseDto>> getShopList(
       @RequestParam(defaultValue = "10") int size,
       @RequestParam String keyword,
       @PageableDefault(sort = "createdAt", direction = Direction.DESC)
@@ -61,31 +57,28 @@ public class ShopController {
 
     Page<ShopResponseDto> page = shopService.getShopList(size, keyword, pageable);
 
-    return ResponseEntity.ok()
-        .body(new ApiResponse<>("가게 목록 조회 성공", HttpStatus.OK.value(), page));
+    return ApiResponse.success(page);
   }
 
   @Secured({Authority.OWNER, Authority.MASTER, Authority.MANAGER})
   @PutMapping("/{shopId}")
-  public ResponseEntity<ApiResponse> updateShop(
+  public ApiResponse updateShop(
       @PathVariable Long shopId,
       @Valid @RequestBody ShopRequestDto shopRequestDto) {
 
     ShopResponseDto shopResponseDto = shopService.updateShop(shopId, shopRequestDto);
 
-    return ResponseEntity.status(HttpStatus.OK)
-        .body(new ApiResponse("가게 수정 성공", HttpStatus.OK.value(), shopResponseDto));
+    return ApiResponse.success(shopResponseDto);
   }
 
   @Secured({Authority.MASTER, Authority.MANAGER})
   @DeleteMapping("/{shopId}")
-  public ResponseEntity<ApiResponse> deleteShop(
+  public ApiResponse deleteShop(
       @PathVariable Long shopId) {
 
     ShopResponseDto shopResponseDto = shopService.deleteShop(shopId);
 
-    return ResponseEntity.status(HttpStatus.OK)
-        .body(new ApiResponse("가게 삭제 성공", HttpStatus.OK.value(), shopResponseDto));
+    return ApiResponse.success(shopResponseDto);
   }
 
 
