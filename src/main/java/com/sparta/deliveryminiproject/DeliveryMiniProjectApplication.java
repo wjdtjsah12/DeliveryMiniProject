7aefbd6c056b90976521f3bc6d2de0af98e12011
nerdved <wjdtjsah12@gmail.com>
2024-11-13 16:05:17 +0900

    fix: PageImpl 관련 Warning 해결 #37

--- src/main/java/com/sparta/deliveryminiproject/DeliveryMiniProjectApplication.java
+++ src/main/java/com/sparta/deliveryminiproject/DeliveryMiniProjectApplication.java
@@ -2,9 +2,11 @@ package com.sparta.deliveryminiproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
 public class DeliveryMiniProjectApplication {
 
   public static void main(String[] args) {
