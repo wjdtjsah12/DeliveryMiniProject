1ef82fe99c3f87c48328a3011b0955bec5102b24
nerdved <wjdtjsah12@gmail.com>
2024-11-14 22:54:05 +0900

    fix: GlobalExceptionHandler Gemini 모델 과부하 대비 예외처리 추가

--- src/main/java/com/sparta/deliveryminiproject/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/sparta/deliveryminiproject/global/exception/GlobalExceptionHandler.java
@@ -2,14 +2,27 @@ package com.sparta.deliveryminiproject.global.exception;
 
 import com.sparta.deliveryminiproject.global.response.ApiResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpServerErrorException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+  @ExceptionHandler(HttpServerErrorException.class)
+  public ResponseEntity<ApiResponse> serverErrorHandler(HttpServerErrorException ex) {
+    HttpStatusCode statusCode = ex.getStatusCode();
+    String errorMessage = ex.getMessage();
+    if (errorMessage.contains("he model is overloaded")) {
+      errorMessage = "모델이 과부하되었습니다. 나중에 다시 시도해 주세요.";
+    }
+    return ResponseEntity.status(statusCode)
+        .body(ApiResponse.error(statusCode.value(), errorMessage));
+  }
 
   @ExceptionHandler(MethodArgumentNotValidException.class)
   public ResponseEntity<ApiResponse> handleValidationException(BindingResult bindingResult) {
