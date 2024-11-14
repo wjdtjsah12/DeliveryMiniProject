51b06fa780e9133a67ea4501024e779a05d69c4c
geon8692 <geon4735@gmail.com>
2024-11-14 11:25:42 +0900

    feat: 주문 생성 API 구현

---
+++ src/main/java/com/sparta/deliveryminiproject/domain/order/dto/OrderRequestDto.java
@@ -0,0 +1,30 @@
+package com.sparta.deliveryminiproject.domain.order.dto;
+
+import com.sparta.deliveryminiproject.domain.order.entity.Order;
+import com.sparta.deliveryminiproject.domain.order.entity.OrderStatus;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class OrderRequestDto {
+
+  private String requests;
+  private String address;
+  private String phoneNumber;
+
+  @Builder
+  private OrderRequestDto(String requests, String address, String phoneNumber) {
+    this.requests = requests;
+    this.address = address;
+    this.phoneNumber = phoneNumber;
+  }
+
+  public Order toEntity(OrderRequestDto orderRequestDto) {
+    return Order.builder()
+        .requests(orderRequestDto.getRequests())
+        .address(orderRequestDto.getAddress())
+        .phoneNumber(orderRequestDto.getPhoneNumber())
+        .orderStatus(OrderStatus.PENDING_PAYMENT)
+        .build();
+  }
+}
