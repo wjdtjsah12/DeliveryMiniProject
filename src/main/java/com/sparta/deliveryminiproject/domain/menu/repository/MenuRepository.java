961fe7086e74de440eecb4d77a5957acaabd7f89
k-jaehyun <wogus146@gmail.com>
2024-11-17 00:21:51 +0900

    feat: Menu 목록 조회 queryDSL 및 fetchJoin 구현

--- src/main/java/com/sparta/deliveryminiproject/domain/menu/repository/MenuRepository.java
+++ src/main/java/com/sparta/deliveryminiproject/domain/menu/repository/MenuRepository.java
@@ -3,16 +3,10 @@ package com.sparta.deliveryminiproject.domain.menu.repository;
 import com.sparta.deliveryminiproject.domain.menu.entity.Menu;
 import com.sparta.deliveryminiproject.global.exception.ApiException;
 import java.util.UUID;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 
-public interface MenuRepository extends JpaRepository<Menu, UUID> {
-
-  Page<Menu> findByMenuNameContainingIgnoreCaseAndIsDeletedFalseAndIsHiddenFalseAndShopId(
-      String searchQuery,
-      Pageable pageable, UUID shopId);
+public interface MenuRepository extends JpaRepository<Menu, UUID>, MenuRepositoryCustom {
 
   default Menu findMenuByIdOrElseThrow(UUID id) {
     return findById(id).orElseThrow(
---
+++ src/main/java/com/sparta/deliveryminiproject/domain/menu/repository/MenuRepositoryImpl.java
@@ -0,0 +1,53 @@
+package com.sparta.deliveryminiproject.domain.menu.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.sparta.deliveryminiproject.domain.menu.entity.Menu;
+import com.sparta.deliveryminiproject.domain.menu.entity.QMenu;
+import com.sparta.deliveryminiproject.domain.shop.entity.QShop;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+public class MenuRepositoryImpl implements MenuRepositoryCustom {
+
+  private final JPAQueryFactory queryFactory;
+
+  public MenuRepositoryImpl(EntityManager em) {
+    this.queryFactory = new JPAQueryFactory(em);
+  }
+
+
+  @Override
+  public Page<Menu> searchMenu(
+      String searchQuery, Pageable pageable, UUID shopId) {
+
+    QMenu menu = QMenu.menu;
+    QShop shop = QShop.shop;
+
+    BooleanExpression searchCondition = menu.menuName.containsIgnoreCase(searchQuery)
+        .and(menu.isDeleted.isFalse())
+        .and(menu.isHidden.isFalse());
+
+    List<Menu> result = queryFactory
+        .selectFrom(menu)
+        .join(menu.shop, shop).fetchJoin()
+        .where(searchCondition)
+        .where(menu.shop.id.eq(shopId))
+        .offset(pageable.getOffset())
+        .limit(pageable.getPageSize())
+        .fetch();
+
+    long total = queryFactory
+        .selectFrom(menu)
+        .where(searchCondition)
+        .where(menu.shop.id.eq(shopId))
+        .fetchCount();
+
+    return new PageImpl<>(result, pageable, total);
+
+  }
+}
