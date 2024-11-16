743c46d7ccb8c17a3ac18686f7d46876f3b3c69e
k-jaehyun <wogus146@gmail.com>
2024-11-16 21:53:43 +0900

    feat: ShopCategory 조회의 N+1 문제 해결 (fetch join)

--- src/main/java/com/sparta/deliveryminiproject/domain/shopCategory/repository/ShopCategoryRepository.java
+++ src/main/java/com/sparta/deliveryminiproject/domain/shopCategory/repository/ShopCategoryRepository.java
@@ -4,8 +4,11 @@ import com.sparta.deliveryminiproject.domain.shopCategory.entity.ShopCategory;
 import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ShopCategoryRepository extends JpaRepository<ShopCategory, UUID> {
 
-  Set<ShopCategory> findAllByShopId(UUID shopId);
+  @Query("SELECT sc FROM ShopCategory sc JOIN FETCH sc.category WHERE sc.shop.id = :shopId")
+  Set<ShopCategory> findAllByShopId(@Param("shopId") UUID shopId);
 }
