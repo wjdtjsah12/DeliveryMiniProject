c86809f45106be1bd0f6e7e8405c5642388c87e9
k-jaehyun <wogus146@gmail.com>
2024-11-16 23:06:41 +0900

    refactor: ShopCategoryRepository queryDSL 적용

--- src/main/java/com/sparta/deliveryminiproject/domain/shopCategory/repository/ShopCategoryRepository.java
+++ src/main/java/com/sparta/deliveryminiproject/domain/shopCategory/repository/ShopCategoryRepository.java
@@ -1,14 +1,10 @@
 package com.sparta.deliveryminiproject.domain.shopCategory.repository;
 
 import com.sparta.deliveryminiproject.domain.shopCategory.entity.ShopCategory;
-import java.util.Set;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface ShopCategoryRepository extends JpaRepository<ShopCategory, UUID> {
+public interface ShopCategoryRepository extends JpaRepository<ShopCategory, UUID>,
+    ShopCategoryRepositoryCustom {
 
-  @Query("SELECT sc FROM ShopCategory sc JOIN FETCH sc.category WHERE sc.shop.id = :shopId")
-  Set<ShopCategory> findAllByShopId(@Param("shopId") UUID shopId);
 }
