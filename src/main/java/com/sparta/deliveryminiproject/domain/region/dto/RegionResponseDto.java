4c41afc8f69e7f8624704510ddf9c492ffb49f59
nerdved <wjdtjsah12@gmail.com>
2024-11-18 14:23:33 +0900

    feat: Region Entity 기본설정 및 CRUD 구현

---
+++ src/main/java/com/sparta/deliveryminiproject/domain/region/dto/RegionResponseDto.java
@@ -0,0 +1,29 @@
+package com.sparta.deliveryminiproject.domain.region.dto;
+
+import com.sparta.deliveryminiproject.domain.region.entity.Region;
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.Getter;
+
+@Getter
+public class RegionResponseDto {
+
+  private UUID id;
+  private String regionName;
+  private LocalDateTime createdAt;
+  private String createdBy;
+  private LocalDateTime updatedAt;
+  private String updatedBy;
+  private Boolean isDeleted;
+
+  public RegionResponseDto(Region region) {
+    this.id = region.getId();
+    this.regionName = region.getRegionName();
+    this.createdAt = region.getCreatedAt();
+    this.createdBy = region.getCreatedBy();
+    this.updatedAt = region.getUpdatedAt();
+    this.updatedBy = region.getUpdatedBy();
+    this.isDeleted = region.getIsDeleted();
+  }
+
+}
